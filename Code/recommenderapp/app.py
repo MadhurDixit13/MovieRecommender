9af5f6209d024b4ee5aa024d5bce39d641c10de8
Madhur Dixit <madhurdixit37@gmail.com>
2023-10-19 12:22:06 -0400

    OMDB API Integration
    Not yet working, issues to be resolved

--- Code/recommenderapp/app.py
+++ Code/recommenderapp/app.py
@@ -9,16 +9,25 @@ sys.path.append("../../")
 from Code.prediction_scripts.item_based import recommendForNewUser
 from search import Search
 
+import requests
+
 app = Flask(__name__)
 app.secret_key = "secret key"
 
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+# Replace 'YOUR_API_KEY' with your actual OMDB API key
+OMDB_API_KEY = 'b726fa05'
 
+def get_movie_info(title):
+    url = f"http://www.omdbapi.com/?t={title}&apikey={OMDB_API_KEY}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
 @app.route("/")
 def landing_page():
     return render_template("landing_page.html")
-
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -27,35 +36,31 @@ def predict():
     training_data = []
     for movie in data1:
         movie_with_rating = {"title": movie, "rating": 5.0}
+        movie_info = get_movie_info(movie)
+        if movie_info:
+            movie_with_rating.update(movie_info)
         training_data.append(movie_with_rating)
     recommendations = recommendForNewUser(training_data)
     recommendations = recommendations[:10]
     resp = {"recommendations": recommendations}
     return resp
 
-
 @app.route("/search", methods=["POST"])
 def search():
     term = request.form["q"]
-    print("term: ", term)
-
     search = Search()
     filtered_dict = search.resultsTop10(term)
-
     resp = jsonify(filtered_dict)
     resp.status_code = 200
     return resp
 
-
 @app.route("/feedback", methods=["POST"])
 def feedback():
     data = json.loads(request.data)
-    with open("experiment_results/feedback_{}.csv".format(int(time.time())), "w") as f:
+    with open(f"experiment_results/feedback_{int(time.time())}.csv", "w") as f:
         for key in data.keys():
-            f.write("%s - %s\n" % (key, data[key]))
-    print(data)
+            f.write(f"{key} - {data[key]}\n")
     return data
-
 
 @app.route("/success")
 def success():
